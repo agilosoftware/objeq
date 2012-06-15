41473c1bc436ebeb2c093990c2f9664728730e8d
bradford653 <thom@kode4food.it>
2012-06-15 09:27:52 +0200

    moved src->objeq

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-jison src/objeq-parser.y -m commonjs -o "src/\$objeqParser.js"
-mv "src/\$objeqParser.js" src/objeq-parser.js
+jison objeq/objeq-parser.y -m commonjs -o "objeq/\$objeqParser.js"
+mv "objeq/\$objeqParser.js" objeq/objeq-parser.js
 
-cat src/objeq-parser.js src/objeq.js > "src/\$objeqBuildInput.js"
-uglifyjs "src/\$objeqBuildInput.js" > objeq.min.js
-rm "src/\$objeqBuildInput.js"
+cat objeq/objeq-parser.js objeq/objeq.js > "objeq/\$objeqBuildInput.js"
+uglifyjs "objeq/\$objeqBuildInput.js" > objeq.min.js
+rm "objeq/\$objeqBuildInput.js"
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./src/objeq');
+module.exports = require('./objeq/objeq');
