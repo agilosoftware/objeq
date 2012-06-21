6bb99317b24bb7e183ae7773839cff1d2b4c010d
bradford653 <thom@kode4food.it>
2012-06-21 13:30:53 +0200

    only $objeq will pollute global namespace... moving build into a node script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-jison objeq/objeq-parser.y -m commonjs -o "objeq/\$objeqParser.js"
-mv "objeq/\$objeqParser.js" objeq/objeq-parser.js
-
-cat objeq/objeq-parser.js objeq/objeq.js > "objeq/\$objeqBuildInput.js"
-uglifyjs "objeq/\$objeqBuildInput.js" > objeq.min.js
-rm "objeq/\$objeqBuildInput.js"
