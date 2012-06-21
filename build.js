8e4c342ceb13216ba0459c4b3ac9d55355d76542
bradford653 <thom@kode4food.it>
2012-06-21 18:07:01 +0200

    formatting like npm

--- build.js
+++ build.js
@@ -2,8 +2,6 @@
 // pollute the global namespace. It should be capable of
 // being used both from the browser and from node.js
 
-console.log("Starting objeq Build:");
-
 var fs = require("fs");
 var jison = require("jison/");
 var IO = require("jison/lib/jison/util/io");
@@ -11,19 +9,33 @@ var bnf = require("jison/lib/jison/bnf");
 var jsp = require("uglify-js").parser;
 var pro = require("uglify-js").uglify;
 
-console.log("* Parsing objeq Grammar"); // ************************************
+var TermCodes = {
+  "error_prefix": "\u001B[31m",
+  "error_suffix": "\u001B[39m",
+  "ok_prefix": "\u001B[32m",
+  "ok_suffix": "\u001B[39m",
+  "bold_prefix": "\u001B[1m",
+  "bold_suffix": "\u001B[22m"
+};
+
+function fmt(type, value) {
+  return TermCodes[type+"_prefix"] + value + TermCodes[type+"_suffix"];
+}
+
+console.log(fmt("bold", "Starting objeq Build"));
+
 try {
   var raw = IO.read(IO.join(IO.cwd(), "objeq/objeq-parser.y"));
   var grammar = bnf.parse(raw);
 }
 catch ( err ) {
-  console.log('Oops! Error reading Grammer');
+  console.log(fmt("error", 'Oops! Error reading Grammer'));
   console.log('Reason: '+err);
   console.log();
   process.exit(1);
 }
+console.log("✔ Parsed objeq Grammar");
 
-console.log("* Generating objeq Parser"); // **********************************
 try {
   var options = {
     moduleType: 'js'
@@ -32,15 +44,16 @@ try {
   var parserCode = parser.generate(options);
 }
 catch ( err ) {
-  console.log('Oops! Error generating Parser');
+  console.log(fmt("error", 'Oops! Error generating Parser'));
   console.log('Reason: '+err);
   console.log();
   process.exit(2);
 }
+console.log("✔ Generated objeq Parser");
 
 var replaceString = "var parser = (function(){";
 if ( parserCode.indexOf(replaceString) === -1 ) {
-  console.log('Oops! Assignment string not found in generated Parser!');
+  console.log(fmt("error", 'Oops! Assignment not found in Parser!'));
   console.log();
   process.exit(3);
 }
@@ -62,26 +75,26 @@ var endCode = "\n" +
 
 var out = parserCode.replace(replaceString, startCode);
 
-console.log("* Writing ./objeq/objeq-parser.js"); // **************************
 fs.writeFile("./objeq/objeq-parser.js", out + endCode, function(err) {
   if ( !err ) return;
-  console.log('Oops! Error writing Parser');
+  console.log(fmt("error", 'Oops! Error writing Parser'));
   console.log('Reason: '+err);
   console.log();
   process.exit(4);
 });
+console.log("✔ Wrote ./objeq/objeq-parser.js");
 
 // Now use Uglify to compress the JavaScript
-console.log("* Minifying Parser and Processor"); // ***************************
 try {
   var objeqCode = fs.readFileSync("./objeq/objeq.js");
 }
 catch ( err ) {
-  console.log('Oops! Error reading objeq Processor Code');
+  console.log(fmt("error", 'Oops! Error reading objeq Processor Code'));
   console.log('Reason: '+err);
   console.log();
   process.exit(5);
 }
+console.log("✔ Minified Parser and Processor");
 
 var origCode = objeqCode + "\n" + parserCode.replace(replaceString, minCode);
 var ast = jsp.parse(origCode);
@@ -89,13 +102,13 @@ ast = pro.ast_mangle(ast);
 ast = pro.ast_squeeze(ast);
 var finalCode = pro.gen_code(ast);
 
-console.log("* Writing ./objeq.min.js"); // ***********************************
 fs.writeFile("./objeq.min.js", finalCode, function(err) {
   if ( !err ) return;
-  console.log('Oops! Error writing Minified Code');
+  console.log(fmt("error", 'Oops! Error writing Minified Code'));
   console.log('Reason: '+err);
   console.log();
   process.exit(6);
 });
+console.log("✔ Wrote ./objeq.min.js");
 
-console.log("Finished!");
+console.log(fmt("ok", "Finished!"));
